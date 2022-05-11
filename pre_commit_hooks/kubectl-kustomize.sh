530d3cc940b9174937e334aa4b1c54b552da0dfa
Chris Fowles <chris.fowles@plenti.com.au>
2022-05-11 18:17:09 +1000

    initial kustomize hook

---
+++ pre_commit_hooks/kubectl-kustomize.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if ! hash kubectl 2>/dev/null; then
+  echo "kubectl not found"
+  exit 1
+fi
+
+dirsToCheck=$(find . -type f -name "kustomization.yaml" -exec dirname "{}" \; |sort -u)
+
+while IFS= read -r dir; do
+  echo "Checking kustomization on $dir"
+  kubectl kustomize "$dir" 1>/dev/null
+  ret=$?
+  if $ret -ne 0; then
+    echo "Kustomize failed on $dir"
+    exit 1
+  fi
+done <<< "$dirsToCheck"

b91786b4675a0cd4800e501e7920293626418eb0
Chris Fowles <chris.fowles@plenti.com.au>
2022-05-11 18:24:58 +1000

    adds kustomize pre-commit hook

--- kubectl-kustomize.sh
+++ kubectl-kustomize.sh
@@ -8,10 +8,9 @@ fi
 dirsToCheck=$(find . -type f -name "kustomization.yaml" -exec dirname "{}" \; |sort -u)
 
 while IFS= read -r dir; do
-  echo "Checking kustomization on $dir"
   kubectl kustomize "$dir" 1>/dev/null
   ret=$?
-  if $ret -ne "0"; then
+  if [ $ret -ne 0 ]; then
     echo "Kustomize failed on $dir"
     exit 1
   fi
